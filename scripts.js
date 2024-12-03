efa1f54abef19eca906820234b212cd5711153c1
CThanhUit <116928596+CThanhUit@users.noreply.github.com>
2024-12-03 09:52:19 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+<script>
+    fetch('/backend/secret.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('output').innerText = data;
+        });
+</script>
